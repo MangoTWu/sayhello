f090560323837a8fac26e28127adeca65059468c
shiehng <shiehng@gmail.com>
2018-11-27 22:21:10 +0800

    完成数据库配置和Jinja2语句删除空白

---
+++ sayhello/config.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+    :author: Mango Wu
+    :url: https://mangowu.me
+    :copyright: © 2018 Mango Wu <shiehng@gmail.com>
+    :license: MIT, see LICENSE for more details.
+"""
+import os
+
+from sayhello import app
+
+dev_db = 'sqlite:////' + os.path.join(os.path.dirname(app.root_path), 'data.db')
+
+SECRET_KEY = os.getenv('SECRET_KEY', 'secret string')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', dev_db)
